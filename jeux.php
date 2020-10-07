1957e9cd96fc119e0aa62e7d0651b5ca3d67229d
matvki <55830806+matvki@users.noreply.github.com>
2020-10-07 14:10:35 +0200

    update
    add the game

---
+++ jeux.php
@@ -0,0 +1,290 @@
+<?php
+session_start();
+// recuperation des infos
+$classe = $_POST['class'];
+$classe2 = $_POST['class2'];
+$name = $_POST['name'];
+$name2 = $_POST['name2'];
+$weapon = $_POST['weapon'];
+$weapon2 = $_POST['weapon2'];
+
+// premier if pour avoir les toutes les infos
+if (($name != "") && ($name2 != "") && ($classe != "") && ($classe2 != "") && ($name != "") && ($name2 != "") && ($weapon != "") && ($weapon2 != "")) {
+    //creation de la classe personnage
+    class Personnage
+    {
+        public $name;
+        function __construct($name)
+        {
+            $this->name = $name;
+        }
+    }
+    //creation de la classe junior qui decoul de personnage
+    class Junior extends Personnage
+    {
+        public $name;
+        function __construct($name)
+        {
+            parent::__construct($name);
+        }
+        function stat()
+        {
+            $stat = array(
+                "name" => $this->name,
+                "hp" => "100",
+            );
+            return $stat;
+        }
+    }
+    //creation de la classe confirme qui decoule de personnage
+    class Confirme extends Personnage
+    {
+        public $name;
+        function __construct($name)
+        {
+            parent::__construct($name);
+        }
+        function stat()
+        {
+            $stat = array(
+                "name" => $this->name,
+                "hp" => "200",
+            );
+            return $stat;
+        }
+    }
+    //creation de la classe veteran qui decoule de la classe personnage
+    class Veteran extends Personnage
+    {
+        public $name;
+        function __construct($name)
+        {
+            parent::__construct($name);
+        }
+        function stat()
+        {
+            $stat = array(
+                "name" => $this->name,
+                "hp" => "150",
+            );
+            return $stat;
+        }
+    }
+    //creation de l'arme sword
+    class Sword
+    {
+        public $dammage;
+        public $defense;
+        function __construct()
+        {
+            $this->dammage = "15";
+            $this->defense = "15";
+        }
+        function stat($stat)
+        {
+            $weapon = array(
+                "dammage" => $this->dammage,
+                "defense" => $this->defense,
+            );
+            array_push( $stat, $weapon);
+
+            return $stat;
+        }
+    }
+    //creation de l'arme shield
+    class Shield
+    {
+        public $dammage;
+        public $defense;
+        function __construct()
+        {
+            $this->dammage = "50";
+            $this->defense = "30";
+        }
+        function stat($stat)
+        {
+            $weapon = array(
+                "dammage" => $this->dammage,
+                "defense" => $this->defense,
+            );
+            array_push( $stat, $weapon);
+
+            return $stat;
+        }
+    }
+    //creation de l'arme spear
+    class Spear
+    {
+        public $dammage;
+        public $defense;
+        function __construct()
+        {
+            $this->dammage = "20";
+            $this->defense = "8";
+        }
+        function stat($stat)
+        {
+            $weapon = array(
+                "dammage" => $this->dammage,
+                "defense" => $this->defense,
+            );
+            array_push( $stat, $weapon);
+
+            return $stat;
+        }
+    }
+    //creation de l'arme bow
+    class Bow
+    {
+        public $dammage;
+        public $defense;
+        function __construct()
+        {
+            $this->dammage = "30";
+            $this->defense = "5";
+        }
+        function stat($stat)
+        {
+            $weapon = array(
+                "dammage" => $this->dammage,
+                "defense" => $this->defense,
+            );
+            array_push( $stat, $weapon);
+            return $stat;
+        }
+    }
+    // creation de la fonction fight1 qui faire taper le personnage 2 puis le 1
+    function fight1($stat, $stat2){
+        //personnage 1 prend des degats
+        $i = rand(0, 10);
+        switch ($i) {
+            case '4':
+                // coup critique
+                $def = $stat['0']['defense'] * 2;
+                $stat['hp'] = $stat['hp'] + ($def - $stat2['0']['dammage']);
+                break;
+            case '8':
+                // pas de defense
+                $def = 0;
+                $stat['hp'] = $stat['hp'] - $stat2['0']['dammage'];
+                break;
+            default:
+                //defense normale
+                $def = $stat['0']['defense'] * 0.5;
+                $stat['hp'] = $stat['hp'] + ($def - $stat2['0']['dammage']);
+                break;
+        }
+        //personnage 2 prend des degats
+        $i = rand(0, 10);
+        switch ($i) {
+            case '4':
+                // coup critique
+                $def = $stat2['0']['defense'] * 2;
+                $stat2['hp'] = $stat2['hp'] + ($def - $stat['0']['dammage']);
+                break;
+            case '8':
+                // pas de defense
+                $def = 0;
+                $stat2['hp'] = $stat2['hp'] - $stat['0']['dammage'];
+                break;
+            default:
+                //defense normale
+                $def = $stat2['0']['defense'] * 0.5;
+                $stat2['hp'] = $stat2['hp'] + ($def - $stat['0']['dammage']);
+                break;
+        }
+        $arr = array($stat,$stat2);
+        return $arr;
+    }
+    // creation de la fonction fight2 qui fait tapper le personnage 1 avant le 2
+    function fight2($stat, $stat2){
+        //personnage 2 prends des degats
+        $i = rand(0, 10);
+        switch ($i) {
+            case '4':
+                // coup critique
+                $def = $stat2['0']['defense'] * 2;
+                $stat2['hp'] = $stat2['hp'] + ($def - $stat['0']['dammage']);
+                break;
+            case '8':
+                // pas de defense
+                $def = 0;
+                $stat2['hp'] = $stat2['hp'] - $stat['0']['dammage'];
+                break;
+            default:
+                //defense normale
+                $def = $stat2['0']['defense'] * 0.5;
+                $stat2['hp'] = $stat2['hp'] + ($def - $stat['0']['dammage']);
+                break;
+        }
+        //personnage 1 ^prend des degats
+        $i = rand(0, 10);
+        switch ($i) {
+            case '4':
+                // coup critique
+                $def = $stat['0']['defense'] * 2;
+                $stat['hp'] = $stat['hp'] + ($def - $stat2['0']['dammage']);
+                break;
+            case '8':
+                // pas de defense
+                $def = 0;
+                $stat['hp'] = $stat['hp'] - $stat2['0']['dammage'];
+                break;
+            default:
+                //defense normale
+                $def = $stat['0']['defense'] * 0.5;
+                $stat['hp'] = $stat['hp'] + ($def - $stat2['0']['dammage']);
+                break;
+        }
+        $arr = array($stat,$stat2);
+        return $arr;
+    }
+    //creation des perssonngae selon parametre choisie
+    //perso 1
+    $soldier = new $classe($name);
+    $soldier->stat();
+    $stat = $soldier->stat();
+    $soldier = new $weapon();
+    $soldier->stat($stat);
+    $stat = $soldier->stat($stat);
+    //perso 2
+    $soldier2 = new $classe2($name2);
+    $soldier2->stat();
+    $stat2 = $soldier2->stat();
+    $soldier2 = new $weapon2();
+    $soldier2->stat($stat2);
+    $stat2 = $soldier2->stat($stat2);
+    //choix de qui commence a tapper
+    if ($stat['hp'] > $stat2['hp']) {
+        //le perso 2 commence a tapper
+        echo "Première manche " . $stat2['name'] . " commence à tapper ⚔︎⚔︎⚔︎<br>  " . $stat['name'] . " prépare ton bouclier 🛡🛡🛡";
+        //boucle de combat
+        while (($stat['hp'] > 0) && ($stat2['hp']) > 0) {
+            fight1($stat, $stat2);
+            $stat = fight1($stat, $stat2)['0'];
+            $stat2 = fight1($stat, $stat2)['1'];
+        }
+    }else{
+        // le perso 1 commence a taper
+        echo "Première manche " . $stat['name'] . " commence à tapper ⚔︎⚔︎⚔︎<br>  " . $stat2['name'] . " prépare ton bouclier 🛡🛡🛡";
+        //boucle de combat
+        while (($stat['hp'] > 0) && ($stat2['hp'] > 0)) {
+            fight2($stat, $stat2);
+            $stat = fight1($stat, $stat2)['0'];
+            $stat2 = fight1($stat, $stat2)['1'];
+        }
+    }
+    //if des resultat
+    if ($stat['hp'] <= 0) {
+        echo "<br>Bravo a " . $stat2['name'] . ",<br>Tu as gagnée !!🥇";
+    }elseif ($stat2['hp'] <= 0) {
+        echo "<br>Bravo a " . $stat['name'] . ",<br>Tu as gagnée !!🥇";
+    } else {
+        echo "<br>Égalité, plus personne n'est debout. 🤝";
+    }
+} else {
+    //si il n'y a pas tout les renseignement cela renvoie a la page de choix avec une alert
+    $_SESSION["error"] = "Selection non valide";
+    header('Location: ./prep.php');
+    exit();
+}
---
+++ prep.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prep jeux</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    if (isset($_SESSION["error"])) { ?>
+        <script>
+            alert("<?= $_SESSION["error"] ?>")
+        </script>
+    <?php
+        session_destroy();
+    }
+    ?>
+    <h1>Choisissez votre personnage</h1>
+    <form action="./jeux.php" method="post">
+    <div>
+        <label for="name">Entrez votre pseudo</label>
+        <input type="text" name="name" id="name">
+        <div>
+            <label for="Junior">Junior</label>
+            <input type="radio" name="class" id="Junior" value="Junior">
+            <label for="Confirme">Confirme</label>
+            <input type="radio" name="class" id="Confirme" value="Confirme">
+            <label for="Veteran">Veteran</label>
+            <input type="radio" name="class" id="Veteran" value="Veteran">
+        </div>
+        <div>
+            <label for="Sword">Sword</label>
+            <input type="radio" name="weapon" id="Sword" value="Sword">
+            <label for="Shield">Shield</label>
+            <input type="radio" name="weapon" id="Shield" value="Shield">
+            <label for="Spear">Spear</label>
+            <input type="radio" name="weapon" id="Spear" value="Spear">
+            <label for="Bow">Bow</label>
+            <input type="radio" name="weapon" id="Bow" value="Bow">
+        </div>
+    </div>
+    <div>
+        <label for="name">Entrez votre pseudo</label>
+        <input type="text" name="name2" id="name">
+        <div>
+            <label for="Junior2">Junior</label>
+            <input type="radio" name="class2" id="Junior2" value="Junior">
+            <label for="Confirme2">Confirme</label>
+            <input type="radio" name="class2" id="Confirme2" value="Confirme">
+            <label for="Veteran2">Veteran</label>
+            <input type="radio" name="class2" id="Veteran2" value="Veteran">
+        </div>
+        <div>
+            <label for="Sword2">Sword</label>
+            <input type="radio" name="weapon2" id="Sword2" value="Sword">
+            <label for="Shield2">Shield</label>
+            <input type="radio" name="weapon2" id="Shield2" value="Shield">
+            <label for="Spear2">Spear</label>
+            <input type="radio" name="weapon2" id="Spear2" value="Spear">
+            <label for="Bow2">Bow</label>
+            <input type="radio" name="weapon2" id="Bow2" value="Bow">
+        </div>
+        <input type="submit" value="Valider">
+    </div>
+    </form>
+</body>
+
+</html>
